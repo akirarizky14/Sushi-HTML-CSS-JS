b3c89edb602aec41fd1c4fe81eeab3ea4da21ca3
akirarizky14 <akr@Akiras-MacBook-Pro.local>
2022-11-19 20:38:30 +0300

    second commit

--- main.js
+++ main.js
@@ -16,3 +16,31 @@ if(navClose){
 }
 
 // Remove Menu
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// Change Background Header
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('bg-header') 
+                       : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== TESTIMONIAL SWIPER ===============*/
+// let testimonialSwiper = new Swiper(".testimonial-swiper", {
+//     spaceBetween: 30,
+//     loop: 'true',
+
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+// });
